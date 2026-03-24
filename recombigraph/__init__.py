587da33d72b0987566615a5fcbb86a00376e8cc1
Patrick McKenzie <Patrick McKenzie>
2026-03-23 22:50:36 -0400

    created initial repo structure

---
+++ recombigraph/__init__.py
@@ -0,0 +1,10 @@
+"""recombigraph: pedigree-based recombination simulation with explicit homolog tracking."""
+
+from .ancestry import Segment, Homolog
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Segment",
+    "Homolog",
+]
